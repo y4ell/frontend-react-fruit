cac62d1ed0ef2eed0c0a2bf17de1c2898177fa8b
y4ell <yaael.s@gmail.com>
2023-11-08 09:43:47 +0100

    begin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteSvgr from "vite-plugin-svgr";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSvgr()],
+  plugins: [react(), svgr()],
 })
+
